3c2a3b42189ea3d2b0740b03441dcca60e867db8
joe-white09 <joe_white09@yahoo.com>
2022-09-08 10:47:12 -0500

    added checkout process

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
+import Success from './pages/Success';
 import { StoreProvider } from './utils/GlobalState';
 import OrderHistory from './pages/OrderHistory';
 
@@ -63,6 +64,10 @@ function App() {
               <Route 
                 exact path="/products/:id" 
                 component={Detail} 
+              />
+              <Route
+                exact path="/success"
+                component={Success}
               />
               <Route 
                 
